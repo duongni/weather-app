44081607c273fa4b6ebe3056fef487e2095510ad
duongni <133175915+duongni@users.noreply.github.com>
2023-05-13 12:09:15 -0400

    Initial Setup for the app

---
+++ sources/script.js
@@ -0,0 +1,3 @@
+let apiKey = a0a06a4d0t0a9fff1oce244a979b7153;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query={New York}&key=${a0a06a4d0t0a9fff1oce244a979b7153}`;
+console.log(apiUrl);
