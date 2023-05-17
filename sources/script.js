f1cf2aa738e7cc73cf6bbd48760da69a2c0f4726
duongni <133175915+duongni@users.noreply.github.com>
2023-05-17 00:16:45 -0400

    Added search bar, search city, change theme.

--- sources/script.js
+++ sources/script.js
@@ -55,8 +55,48 @@ function showTemp(response) {
   );
 }
 
-let apiKey = "a0a06a4d0t0a9fff1oce244a979b7153";
-let city = "Mississippi";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-let imagesPath = "sources/images/";
-axios.get(apiUrl).then(showTemp);
+function searchCity(city) {
+  let apiKey = "a0a06a4d0t0a9fff1oce244a979b7153";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function handleInput(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#city-input");
+  searchCity(cityInput.value);
+}
+
+searchCity("New York");
+
+let citySearch = document.querySelector("#citySearchBar");
+citySearch.addEventListener("submit", handleInput);
+
+function searchLocation(position) {
+  let apiKey = "8161b4309ee03faae957729ba7104797";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apikKey}`;
+
+  https: axios.get(apiUrl).then(showTemp);
+}
+function searchCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-search");
+currentLocationButton.addEventListener("click", searchCurrentLocation);
+
+function changeTheme() {
+  let body = document.querySelector("body");
+  body.classList.toggle("dark");
+}
+let now = new Date();
+let hour = now.getHours();
+if (hour > 18) {
+  changeTheme();
+}
+if (hour < 6) {
+  changeTheme();
+}
